c13c5ac62b963294cde697dd37b645a8194bea65
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-06-04 19:41:38 +0300

    task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,18 +7,20 @@ from flask import jsonify, abort
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def statusview() -> str:
-    """ The line command [GET /api/v1/status]
+    """
+    The line command [GET /api/v1/status]
     Return:
-      - the API status
+    the API status
     """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def statsview() -> str:
-    """ The line command [GET /api/v1/stats]
+    """
+    The line command [GET /api/v1/stats]
     Return:
-      - the objects number
+    the objects number
     """
     from models.user import User
     stats = {}
@@ -28,16 +30,18 @@ def statsview() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorizedaccess() -> None:
-    """ the line of command [GET /api/v1/unauthorized]
+    """
+    the line of command [GET /api/v1/unauthorized]
     Return:
-      - the API status
+    the API status
     """
     abort(401)
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def unauthenticatedaccess() -> None:
-    """the line of code is [GET /api/v1/forbidden]
+    """
+    the line of code is [GET /api/v1/forbidden]
     Return:
       - the API status
     """
