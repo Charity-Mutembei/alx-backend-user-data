b728c99eb43403f836150b5e5eaf1a7cb233da81
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-06-08 23:54:38 +0300

    efactor

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,28 +2,26 @@
 """
 Vew that handles all routes for the Session authentication.
 """
-from os import getenv
 from typing import Tuple, Any
-
-from api.v1.views import app_views
+from os import getenv
 from flask import abort, jsonify, request
 from models.user import User
+from api.v1.views import app_views
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> Tuple[Any, int]:
     """
-
-    :return:
+    task 2
     """
 
-    email = request.form.get('email')
     password = request.form.get('password')
+    email = request.form.get('email')
 
-    if not email:
-        return jsonify({"error": "email missing"}), 400
     if not password:
         return jsonify({"error": "password missing"}), 400
+    if not email:
+        return jsonify({"error": "email missing"}), 400
 
     users = User.search({'email': email})
 
@@ -35,8 +33,8 @@ def login() -> Tuple[Any, int]:
             from api.v1.auth.session_auth import SessionAuth
             session_auth = SessionAuth()
             session_id = session_auth.create_session(user.id)
-            response = jsonify(user.to_json())
             response.set_cookie(getenv('SESSION_NAME'), session_id)
+            response = jsonify(user.to_json())
             return response
         else:
             return jsonify({"error": "wrong password"}), 401
@@ -46,8 +44,7 @@ def login() -> Tuple[Any, int]:
                  strict_slashes=False)
 def logout() -> Tuple[Any, int]:
     """
-
-    :return:
+    task 2
     """
 
     from api.v1.auth.session_auth import SessionAuth
@@ -56,6 +53,6 @@ def logout() -> Tuple[Any, int]:
     if not session_id:
         abort(404)
     else:
-        response = jsonify({}, 200)
         response.set_cookie(getenv('SESSION_NAME'), '')
+        response = jsonify({}, 200)
     return response
