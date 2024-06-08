c3dc20bd694310c75e6b15406567f161ad798b70
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-06-09 00:05:00 +0300

    efactor

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,76 +1,59 @@
 #!/usr/bin/env python3
 """
-Class to manage the API authentication
+Task 4: Session cookie
 """
 from os import getenv
-from typing import List, TypeVar
-
 from flask import request
+from typing import List, TypeVar
 
 
 class Auth:
     """
-    Auth class
+    New class = Class Auth
     """
 
     def __int__(self):
         pass
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    def require_auths(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Determines whether a given path requires authentication or not
-        Args:
-            - path(str): Url path to be checked
-            - excluded_paths(List of str): List of paths that do not require
-              authentication
-        Return:
-            - True if path is not in excluded_paths, else False
+        Task 4: Session cookie
         """
         if path is None:
             return True
-        elif excluded_paths is None or excluded_paths == []:
-            return True
         elif path in excluded_paths:
             return False
+        elif excluded_paths is None or excluded_paths == []:
+            return True
         else:
             for i in excluded_paths:
-                if i.startswith(path):
-                    return False
                 if path.startswith(i):
+                    return False
+                if i.startswith(path):
                     return False
                 if i[-1] == "*":
                     if path.startswith(i[:-1]):
                         return False
         return True
 
-    def authorization_header(self, request=None) -> str:
+    def authorization_headers(self, request=None) -> str:
         """
-        Returns None
-        Args:
-            request:
-
-        Returns:
-
+        Task 4
         """
         if request is None:
             return None
 
         return request.headers.get('Authorization')
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_users(self, request=None) -> TypeVar('User'):
         """
-        Returns None
-        Args:
-            request:
-
-        Returns:
-
+        Task 4
         """
         return None
 
-    def session_cookie(self, request=None):
+    def session_cookies(self, request=None):
         """
-        Returns a cookie value from a request
+        Task 4
         """
         if request is None:
             return None
