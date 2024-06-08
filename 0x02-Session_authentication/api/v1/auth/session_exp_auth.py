e6270a784d2fc80fcfd1cecf299b5b853b212032
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-06-09 00:18:52 +0300

    efactor

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -27,25 +27,25 @@ class SessionExpAuth(Auth):
         """
         task 9
         """
-        if user_id is None or not isinstance(user_id, str):
-            return None
         id = super().create_session(user_id)
         if id is None:
             return None
         self.user_id_by_session_id[id] = {"user_id": user_id, "created_at": time.time()}
+        if user_id is None or not isinstance(user_id, str):
+            return None
         return id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
         Task 9
         """
-        if session_id is None or not isinstance(session_id, str):
-            return None
         session = self.user_id_by_session_id.get(session_id)
         if session is None:
             return None
-        if self.session_duration <= 0:
-            return session.get("user_id")
+        if session_id is None or not isinstance(session_id, str):
+            return None
         if time.time() - session.get("created_at") > self.session_duration:
             return None
+        if self.session_duration <= 0:
+            return session.get("user_id")
         return session.get("user_id")
