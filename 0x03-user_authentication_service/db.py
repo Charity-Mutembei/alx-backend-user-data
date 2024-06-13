ef64713cc98aa9f9c5a07a112c198c58751ad4d0
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-06-14 00:44:54 +0300

    task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 """
-DB module
+Task 1 - create user
 """
-from sqlalchemy import create_engine
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -34,7 +33,8 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database
+        """
+        Add a new user to the database
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
@@ -42,17 +42,18 @@ class DB:
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Find a user by keyword arguments
         """
-        if not kwargs:
-            raise InvalidRequestError()
+        Find the new user using keyword arguments
+        """
         user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
             raise NoResultFound()
+        if not kwargs:
+            raise InvalidRequestError()
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Update a user by id
+        """Update any user by their id
         """
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
